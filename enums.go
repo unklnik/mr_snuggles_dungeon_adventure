b94e670194fb8cc8a5c78607571f3906f9f73541
graves501 <graves501@protonmail.com>
2024-09-19 21:58:57 +0200

    refactor: move structs to objects.go, introduce enums, rename variables

---
+++ enums.go
@@ -0,0 +1,19 @@
+package main
+
+type Ore int
+
+const (
+	PLATINUM Ore = iota
+	GOLD
+	SILVER
+	BRONZE
+	LEAD
+)
+
+const (
+	LEAD_PRICE     = 20
+	BRONZE_PRICE   = 25
+	SILVER_PRICE   = 30
+	GOLD_PRICE     = 40
+	PLATINUM_PRICE = 50
+)
